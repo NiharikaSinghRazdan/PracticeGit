c8d4e30dc7186223d925d3c2385a130f8b960f5a
NiharikaSinghRazdan <niharika.singh.global@gmail.com>
2020-07-19 18:38:17 +0530

    Included security group integration with instance

--- Single-Server_Launch/EC2-Instance.tf
+++ Single-Server_Launch/EC2-Instance.tf
@@ -2,12 +2,15 @@ provider "aws"{
     profile = var.profile
     region=var.region
 }
+
 resource "aws_instance" "FirstInstance"{
     ami= var.ami
     instance_type= var.instance
     key_name="AnsibleKeyPair"
-  #  vpc_security_group_ids=[data.aws_security_group.Port80and22.id]
+# In document it is mentioned to include security group id but it doesn't work. You have to include name 
+# instead of id then only it will work.
+    security_groups=[aws_security_group.Port80and22.name]
     tags= {
-    Name="FirstInstance1"
+    Name="FirstInstance"
 }
 }
--- Single-Server_Launch/Security_groups.tf
+++ Single-Server_Launch/Security_groups.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "Port80and22"{
         from_port=80
         to_port=80
         protocol="tcp"
-        cidr_blocks=[var.mysystem]
+        cidr_blocks=["172.31.0.0/16"]
 
     }
     
@@ -11,6 +11,23 @@ resource "aws_security_group" "Port80and22"{
         from_port=22
         to_port=22
         protocol="tcp"
-        cidr_blocks=[var.mysystem]
+        cidr_blocks=["172.31.0.0/16"]
     }
+    egress{
+        from_port=80
+        to_port=80
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+
+    }
+    
+    egress{
+        from_port=22
+        to_port=22
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }    
+    tags = {
+        Name = "Port80and22"
+        }
 }
