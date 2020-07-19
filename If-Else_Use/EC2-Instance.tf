6e1f8b084772b6a050d5e108f7195a71e8c3dce5
NiharikaSinghRazdan <niharika.singh.global@gmail.com>
2020-07-19 12:55:33 +0530

    Included new file to include if else code

--- If-Else_Use/EC2-Instance.tf
+++ If-Else_Use/EC2-Instance.tf
@@ -1,8 +1,12 @@
+# In this code, we have included if else condition on instance type. If the passed type is t2.micro then 
+# launch 2 instances else launch 1
+
 resource "aws_instance" "EC2-Instance"{
     instance_type=var.instance
     ami=var.ami
     key_name="AnsibleKeyPair"
     availability_zone=var.az
+    # specified if else condition here
     count=(var.instance=="t2.micro" ? 2:1)
     tags ={
         Name="Instance-${count.index}"
